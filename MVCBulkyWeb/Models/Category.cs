3e606fdde44951397e04df54a16d4593707f1b9c
Shashank Dhakate <shashankdkte@gmail.com>
2024-03-10 19:58:59 +0530

    Model and Data Annotations

---
+++ MVCBulkyWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MVCBulkyWeb.Models
+{
+	public class Category
+	{
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
