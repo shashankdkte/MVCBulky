b153a659ffa73c59c933aa96cabc915101454ae2
Shashank Dhakate <shashankdkte@gmail.com>
2024-03-10 19:29:31 +0530

    Add project files.

---
+++ MVCBulkyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCBulkyWeb.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
