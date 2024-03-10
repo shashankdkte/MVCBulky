efc722bc9bd0ce08ab3fd5bd14cc87a8c2a12a9c
Shashank Dhakate <shashankdkte@gmail.com>
2024-03-10 20:16:01 +0530

    Adding Category Table To Db

--- MVCBulkyWeb/Models/Category.cs
+++ MVCBulkyWeb/Models/Category.cs
@@ -1,10 +1,13 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 
 namespace MVCBulkyWeb.Models
 {
 	public class Category
 	{
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string  Name { get; set; }
         public int DisplayOrder { get; set; }
     }
