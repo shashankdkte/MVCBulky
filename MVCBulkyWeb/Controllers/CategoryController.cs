16e8362d31330c136d3ab9356c3cf4635b74c077
Shashank Dhakate <shashankdkte@gmail.com>
2024-03-11 13:14:39 +0530

    CRUD Category

--- MVCBulkyWeb/Controllers/CategoryController.cs
+++ MVCBulkyWeb/Controllers/CategoryController.cs
@@ -25,14 +25,70 @@ namespace MVCBulkyWeb.Controllers
 		[HttpPost]
 		public IActionResult Create(Category category)
 		{
+			//ModelState.AddModelError("Name","")
 			if(ModelState.IsValid)
 			{
 
 			_db.Categories.Add(category);
 			_db.SaveChanges();
-			return RedirectToAction("Index");
+                TempData["success"] = "Category Added Successfully";
+                return RedirectToAction("Index");
 			}
 			return View();
 		}
-	}
+        public IActionResult Edit(int? id)
+        {
+			if(id == null || id ==0) {
+				return NotFound();
+			}
+
+			Category category = _db.Categories.FirstOrDefault(c => c.Id == id);
+			if(category == null)
+			{
+				return NotFound();
+			}
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            //ModelState.AddModelError("Name","")
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            //ModelState.AddModelError("Name","")
+            Category category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            _db.Categories.Remove(category);
+                _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+                return RedirectToAction("Index");
+            
+        }
+    }
 }
